dbcea11f07cdd058726780474596399b070a56b7
davezer <davezer27@gmail.com>
2021-12-06 18:12:55 -0500

    setup something to do something

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,14 +8,28 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 
-app.get('/', (req, res) => {
-    res.json({
-        message: 'Sup world?'
-    });
-});
+
 
 app.use((req, res) => {
     res.status(404).end();
+});
+
+// connect to db
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+
+        user: 'root',
+
+        password: 'ssTbAmory!203CoC?',
+        database: 'election'
+    },
+    console.log('Connected to the election database')
+)
+
+db.query(`SELECT * FROM candidates`, (err, rows) =>{
+    console.log(rows);
 })
 
 app.listen(PORT, () => {
